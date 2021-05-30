ea845596c3636e3f6fd5208a50488889a4101647
Ruchir Rathore <rathoreruchir2@gmail.com>
2021-05-30 13:16:50 +0530

    initial backend setup

---
+++ app.js
@@ -0,0 +1,74 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('./routes/cors');
+require('dotenv').config();
+var passport = require('passport');
+var authenticate = require('./authenticate');
+var config = require('./config');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var profileRouter = require('./routes/profile')
+
+var app = express();
+app.use(cors.cors);
+
+
+//mongoose connection
+const mongoose = require('mongoose');
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB CONNECTED"))
+  .catch((err) => console.log("DB CONNECTION ERR", err));
+
+  
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+if(process.env.NODE_ENV == "production"){
+  app.use(express.static(path.join(__dirname, "client/build")))
+
+  app.get('/*', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); //relative path
+})
+}
+
+//Passport initialization for authentication
+app.use(passport.initialize());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/profile', profileRouter)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
